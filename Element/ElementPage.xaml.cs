db18da8f70311ea7335645b1194ac9144377c0ee
Kibnet Philosoff <kibnet@hotmail.com>
2017-03-25 23:52:20 +0300

    Добавил ReactiveUI и форму авторизации

--- Element/ElementPage.xaml.cs
+++ Element/ElementPage.xaml.cs
@@ -7,6 +7,7 @@ namespace Element
 		public ElementPage()
 		{
 			InitializeComponent();
+			BindingContext = new ElementViewModel();
 		}
 	}
 }
---
+++ Element/ElementViewModel.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Element
+{
+	public class ElementViewModel:ReactiveObject
+	{
+		public ElementViewModel()
+		{
+			SignIn = ReactiveCommand.CreateFromTask(async () =>
+			{
+				try
+				{
+					IsBusy = true;
+					await Task.Delay(1000);
+					token = "token";
+					Status = $"Sign in by {Login}";
+				}
+				catch(Exception e)
+				{
+					
+				}
+				finally
+				{
+					IsBusy = false;
+				}
+			}, this.WhenAny(m=>m.Login, m=>m.Password, m=>m.token, (log, pas, tok) => log.Value!=null && pas.Value!=null && tok.Value==null));
+			SignOut = ReactiveCommand.CreateFromTask(async () =>
+			{
+				token = null;
+				Status = $"Sign out by {Login}";
+			}, this.WhenAny(m => m.token, (tok) => tok.Value != null));
+			this.ObservableForProperty(m=>m.token).Subscribe((obj) => 
+			{
+				if(token!=null)
+				{
+					Password = null;
+					ShowLogin = false;
+					ShowControl = true;
+				}
+				else
+				{
+					ShowControl = false;
+					ShowLogin = true;
+				}
+			});
+			Lock = ReactiveCommand.Create(() => Status = "Locked");
+			Unlock = ReactiveCommand.Create(() => Status = "Unlocked");
+		}
+
+		[Reactive]
+		public string Status { get; set;}
+
+		public ReactiveCommand Lock { get; set; }
+
+		public ReactiveCommand Unlock { get; set; }
+
+
+		public bool? GuardMode { get; set; }
+
+		public bool? ServiceMode { get; set; }
+
+		[Reactive]
+		public bool IsBusy { get; set; } = false;
+
+
+		[Reactive]
+		private string token { get; set; }
+
+		public ReactiveCommand SignIn { get; set; }
+
+		public ReactiveCommand SignOut { get; set; }
+
+		[Reactive]
+		public string Login { get; set; }
+
+		[Reactive]
+		public string Password { get; set; }
+
+		[Reactive]
+		public bool ShowLogin { get; set; } = true;
+
+		[Reactive]
+		public bool ShowControl { get; set; }
+
+	}
+}
